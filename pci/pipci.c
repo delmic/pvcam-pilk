e4d7d94701dfd152e874a98fa5b798e9c99cc787
Éric Piel <piel@delmic.com>
2013-02-22 09:44:24 +0100

    mark the code as GPL v2
    Rob Allen from Princeton Instrument (RAllen@princetoninstruments.com) told me
    that the code is now GPL v2, and asked me to update the license information:
    
    > It should have the same license as the USB driver, feel free to
    > change it to "GPL v2" and add the comments at the top that states
    > that it is free software.  I was going to do it by was too lazy to
    > make a github account and branch the code.
    >
    > Rob

--- pci/pipci.c
+++ pci/pipci.c
@@ -1,3 +1,22 @@
+/*
+ * pipci.c
+ *
+ * Copyright (C) 2002, 2008 Princeton Instruments
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 2 of the License
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
 /*************************************************************
 **************************************************************
 ***     Added version info which is available              ***
@@ -75,7 +94,7 @@
 	MODULE_PARM_DESC( IRQ,"Specify IRQ to use for pipci");
 	MODULE_PARM_DESC( SHARE,"1 Enables Irq Sharing, 0 Disables Irq Sharing" );
 
-	MODULE_LICENSE( "Proprietary" );
+	MODULE_LICENSE( "GPL v2" );
 
 	/*-------------DRIVER ENTRY ROUTINES--------------------*/
 	
